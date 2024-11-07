a53261ac1a5a77b974d8ae5b7f483615a55c735d
Kenneth Tin Yuet C. <60503179+urbanclimatefr@users.noreply.github.com>
2024-11-07 13:05:08 -0500

    Add files via upload

---
+++ 01_Create_mapped_index.py
@@ -0,0 +1,23 @@
+from elasticsearch import Elasticsearch
+
+
+es = Elasticsearch("http://localhost:9200")
+
+request_body = {
+	    "settings" : {
+	        "number_of_shards": 1,
+	        "number_of_replicas": 1
+	    },
+	    'mappings': {
+	            'properties': {
+	                'process': {'type': 'text'},
+	                'date': {'format': 'yyyy-MM-dd', 'type': 'date'},
+	                'processing_time': {'type': 'double'},
+	                'records': {'type': 'integer'},
+					'run': {'type': 'integer'},
+					'msg': {'type': 'text'}
+	            }}
+	}
+
+print("creating 'example_index' index...")
+es.indices.create(index = 'etl_monitoring', body = request_body)
